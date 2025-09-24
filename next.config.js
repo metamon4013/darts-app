6f2c876527f13bd48b7131f8daf3ef0807fa5f55
metamon4013 <satoshi.noguchi4013@gmail.com>
2025-09-25 01:44:22 +0900

    feat: Setting for exe

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development';
 const nextConfig = {
   output: 'export',
+  assetPrefix: isDev ? '' : './',
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // 型エラー無視
   },
   webpack: (config) => {
     config.resolve.fallback = {
